edcf6372017e796fe282c1ed5014b713c0271a49
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-10-30 17:04:42 +0100

    you

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Create a class LRUCache that inherits from BaseCaching"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+    access_check = []
+
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Must assign to the dictionary self.cache_data"""
+        if key and item:
+            self.cache_data[key] = item
+            if key in self.access_check:
+                self.access_check.append(
+                        self.access_check.pop(
+                            self.access_check.index(key)
+                        )
+                    )
+            else:
+                self.access_check.append(key)
+            if len(self.access_check) > BaseCaching.MAX_ITEMS:
+                k = self.access_check.pop(0)
+                self.cache_data.pop(k)
+                print(f"DISCARD: {k}")
+
+    def get(self, key):
+        """Return the value in self.cache_data"""
+        if key in self.cache_data:
+            self.access_check.append(
+                        self.access_check.pop(
+                            self.access_check.index(key)
+                        )
+                    )
+        return self.cache_data.get(key, None)
