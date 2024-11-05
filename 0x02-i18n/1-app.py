cc43ca1b7fa81e264adc21134edec406c9c91e15
Agiamah Mary Uyoyooghene <under20techie@gmail.com>
2024-11-05 15:40:51 +0100

    you

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""setup a basic Flask app"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from config import Config
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app, default_locale=Config.LANGUAGES[0], timezone="UTC")
+
+
+@app.route('/')
+def home():
+    """Return 0-index.html content"""
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
